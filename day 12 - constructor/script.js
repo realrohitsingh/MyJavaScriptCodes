5f740af1120ddb287ceea900e70215167600c913
rohit-Singh18 <rohitsingh888399@gmail.com>
2025-08-18 11:43:40 +0530

    my changes

--- day 12 - constructor/script.js
+++ day 12 - constructor/script.js
@@ -36,3 +36,39 @@ console.log(s1);
 console.log(s1.name);
 console.log(s1.marks[1]);
 console.log(s1.isPresent);
+
+// Write a program to accept user information of name,id,phone,email,password for facebook using class?
+// Write a program to accept properties of a car like name,id,model,engine,color and segregrte their values into array using inbult function?
+
+class User {
+  constructor(name, id, phone, email, password) {
+    this.name = name;
+    this.id = id;
+    this.phone = phone;
+    this.email = email;
+    this.password = password;
+  }
+}
+let user1 = new User(
+  "John Doe",
+  12345,
+  "9876543210",
+  "johndoe1@gmail.com",
+  "password123"
+);
+console.log(user1);
+
+class Car {
+  constructor(name, id, model, engine, color) {
+    this.name = name;
+    this.id = id;
+    this.model = model;
+    this.engine = engine;
+    this.color = color;
+  }
+}
+let car1 = new Car("Toyota", 101, "Corolla", "1.8L", "Blue");
+let car2 = new Car("Honda", 102, "Civic", "2.0L", "Red");
+let car3 = new Car("Ford", 103, "Focus", "2.0L", "Black");
+let carArray = [car1, car2, car3];
+console.log(carArray);
