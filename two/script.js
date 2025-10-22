2e41b4ff60cff14d3ed88eff85f1ab92beaf0935
rohit-Singh18 <rohitsingh888399@gmail.com>
2025-10-22 23:30:31 +0530

    update

--- two/script.js
+++ two/script.js
@@ -1,7 +1,7 @@
-let a = 10;
-let b = ++a;
-let res = --b + a++ + --a;
-console.log(res);
+// let a = 10;
+// let b = ++a;
+// let res = --b + a++ + --a;
+// console.log(res);
 
 // let a = 10;
 // console.log(a++ == a);
@@ -28,3 +28,29 @@ console.log(res);
 // let res = outer();
 // res();
 // res();
+
+// let str = "this is javascript";
+// str = str.split(" ").reverse().join(" ");
+// console.log(str);
+
+// let name = "Rohit";
+// let isVowel = 'aeiou'.includes(name.charAt(0).toLowerCase());
+// console.log(isVowel);
+
+// let arr = [10,20,30];
+// let res = arr.filter((el)=> {
+//     return el*el>500;
+
+// })
+// console.log(res);
+
+// let arr = [10, 20, 30];
+// let res = arr.map(el => el * el);
+// console.log(res);
+
+
+for (let i = 0; i <= 5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    });
+}
